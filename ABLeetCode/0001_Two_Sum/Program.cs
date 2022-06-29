b13bc23c59124b719ec318b9d825a67ca3e5e736
Anh Bui <anh.bui@pinewood.co.uk>
2022-06-29 16:54:28 +0100

    0001 Two Sum

---
+++ ABLeetCode/0001_Two_Sum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+
+var result = new Solution().TwoSum(new int[] { 3, 2, 4 }, 6);
+
+foreach (var item in result)
+{
+    Console.Write($"{item},");
+}
+
+
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        int firstIndex = -1;
+
+        while (firstIndex++ < nums.Length)
+        {
+            for (int i = firstIndex + 1; i < nums.Length; i++)
+            {
+                if (nums[firstIndex] + nums[i] == target)
+                {
+                    return new int[] { firstIndex, i };
+                }
+            }
+        }
+
+        return Array.Empty<int>();
+    }
+}
